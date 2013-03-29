b2bdf0ad0c8a2004d1db40c2cde2f807f808679b
Yao Li <yao@freelab.(none)>
2013-03-29 14:58:35 -0400

    update pos result bbox calculation

--- virat_data_trial.m
+++ virat_data_trial.m
@@ -2,17 +2,15 @@ function [pos, rec_tmp] = virat_data_trial()
   % % % positive process part
   % img_files = '~/Projects/object_detection/dataset/VIRAT_video_cut';
   img_files = '~/Desktop/VIRAT_video_cut2';
-  anno_files = dir('~/Projects/object_detection/tools/VIRATdevkit/VIRAT_Video/Annotations/*.txt');
+  % anno_files = dir('~/Projects/object_detection/tools/VIRATdevkit/VIRAT_Video/Annotations/*.txt');
   % anno_files = dir([VIRATopts.annopath '/*.txt']);
+  anno_files = dir('~/Projects/object_detection/tools/VIRATdevkit/VIRAT_Video/Annotations_tmp/*.txt');
+  
   num_anno_files = length(anno_files);
   numpos = 0;
   % read annotation file by file
-  for i = 1 : 1%num_anno_files
+  for i = 1 : num_anno_files
     rec_tmp = load(anno_files(i).name);
-%     fileID = fopen(anno_files(i).name);
-%     formatSpec = '%s';
-%     N = 8;
-%     rec_tmp = textscan(fileID, formatSpec, N);
     [row col] = size(rec_tmp);
     % read line by line, row is the length of each anno_file
     for j = 1 : row
@@ -20,12 +18,12 @@ function [pos, rec_tmp] = virat_data_trial()
       % extract part before ‘.viratdata.objects.txt’, namely the video name,
       % e.g. filename == VIRAT_S_000001.viratdata.objects.txt
       folder_name = ['/' anno_files(i).name(1 : end-22) '/'];
-      file_name = num2str(rec_tmp(i, 3), '%.6d'); 
+      file_name = num2str(rec_tmp(j, 3), '%.6d'); 
       pos(numpos).im = [img_files folder_name file_name '.jpg'];
-      pos(numpos).x1 = rec_tmp(i, 4);
-      pos(numpos).y1 = rec_tmp(i, 5);
-      pos(numpos).x2 = rec_tmp(i, 4) + rec_tmp(i, 6);
-      pos(numpos).y2 = rec_tmp(i, 5) + rec_tmp(i, 7);
+      pos(numpos).x1 = rec_tmp(j, 4);
+      pos(numpos).y1 = rec_tmp(j, 5);
+      pos(numpos).x2 = rec_tmp(j, 4) + rec_tmp(j, 6);
+      pos(numpos).y2 = rec_tmp(j, 5) + rec_tmp(j, 7);
     end
   end  
 
