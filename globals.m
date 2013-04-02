9283fab4aba25ff8772b4d80ed65dbadbe94954c
Yao <yli44@buffalo.edu>
2013-04-01 23:05:47 -0400

    fix bug for cache directory

--- globals.m
+++ globals.m
@@ -1,7 +1,7 @@
 % Set up global variables used throughout the code
 
 % directory for caching models, intermediate data, and results
-cachedir = '~/viratcache/';
+cachedir = '~/Projects/object_detection/tools/VIRATdevkit/viratcache/';
 
 % directory for LARGE temporary files created during training
 tmpdir = '/var/tmp/virat/';
@@ -10,18 +10,18 @@ tmpdir = '/var/tmp/virat/';
 dataset = '.';
 
 % directory with VIRAT development kit and dataset
-VIRATdevkit = [ dataset ];
+VIRATdevkit = dataset;
 
 % which development kit is being used
 % this does not need to be updated
-VIRATdevkit = false;
-TSUdevkit = false;
-PSUdevkit = false;
-switch dataset
-  case '.'
-    VIRATdevkit=true;
-  case '~/TSU_Experiment'
-    TSUdevkit=true;
-  case '~/PSU_Video'
-    PSUdevkit=true;
-end
+% VIRATdevkit = false;
+% TSUdevkit = false;
+% PSUdevkit = false;
+% switch dataset
+%   case '.'
+%     VIRATdevkit=true;
+%   case '~/TSU_Experiment'
+%     TSUdevkit=true;
+%   case '~/PSU_Video'
+%     PSUdevkit=true;
+% end
