9f2eb660499ff4c64d9e0dbcf091a8ba7ce0ac99
Yao <yli44@buffalo.edu>
2013-04-09 16:47:53 -0400

    update and fix bug in VIRAT data import

--- virat_data_beta.m
+++ virat_data_beta.m
@@ -1,4 +1,4 @@
-function [pos, neg] = virat_data_trial(cls)
+function [pos, neg] = virat_data_beta(cls)
   % [pos, neg] = virat_data(cls)
   % Get training data from the VIRAT dataset.
 
@@ -6,68 +6,26 @@ function [pos, neg] = virat_data_trial(cls)
   virat_init;
 
   % % % positive process part
-  % img_files = '~/Projects/object_detection/dataset/VIRAT_video_cut';
   img_files = '~/Desktop/VIRAT_video_cut2';
-%   anno_files = dir('~/Projects/object_detection/tools/VIRATdevkit/VIRAT_Video/Annotations_tmp1/*.txt');
-%   anno_files = dir('~/Projects/object_detection/tools/VIRATdevkit/VIRAT_Video/Annotations_tmp/*.txt');
-  anno_files = dir('~/Projects/object_detection/tools/VIRATdevkit/VIRAT_Video/Annotations/*.txt');
-  % anno_files = dir([VIRATopts.annopath '/*.txt']);
+  anno_files = dir('~/Projects/object_detection/tools/VIRATdevkit/VIRAT_Video/Annotations_tmp2/*.txt');
+
     
   num_anno_files = length(anno_files);
   numpos = 0;
-  numneg = 0;
+  numneg = 0;  
   pos = [];
   neg = [];
+
   % read annotation file by file
   for i = 1 : num_anno_files
+  
     rec_tmp = load(anno_files(i).name);
     
     [pos_tmp, neg_tmp] = pos_neg_init_process(rec_tmp, cls);
+     
+    [pos, numpos] = pos_process(pos, pos_tmp, numpos, i, img_files, anno_files);
+    [neg, numneg] = neg_process(neg, neg_tmp, numneg, i, img_files, anno_files);
     
-    [row col] = size(rec_tmp);
-    % read line by line, row is the length of each anno_file
-    for j = 1 : row
-      switch cls
-          case 'person'
-              pos = rec_tmp( rec_tmp(:,8)==1, 3:7 );
-              neg_tmp = rec_tmp( rec_tmp(:,8)~=1, 3:7 );
-              frame_pos = pos(:, 1);
-              frame_neg_tmp1 = neg_tmp(:, 1);
-              frame_neg_tmp2 = setxor(frame_neg_tmp1, intersect(frame_neg_tmp1, frame_pos));
-              frame_neg = ismember(frame_neg_tmp1, frame_neg_tmp2);
-              neg = neg_tmp(frame_neg, :);
-              
-          case 'vehicle'
-              pos = rec_tmp( ismember(rec_tmp(j, 8), [2,3]), 3:7 );
-              neg_tmp = rec_tmp( rec_tmp(:,8)~=1, 3:7 );
-              frame_pos = pos(:, 1);
-              frame_neg_tmp1 = neg_tmp(:, 1);
-              frame_neg_tmp2 = setxor(frame_neg_tmp1, intersect(frame_neg_tmp1, frame_pos));
-              frame_neg = ismember(frame_neg_tmp1, frame_neg_tmp2);
-              neg = neg_tmp(frame_neg, :);
-              
-          case 'car'
-              pos = rec_tmp( rec_tmp(:,8)==2, 3:7 );
-              neg_tmp = rec_tmp( rec_tmp(:,8)~=2, 3:7 );
-              frame_pos = pos(:, 1);
-              frame_neg_tmp1 = neg_tmp(:, 1);
-              frame_neg_tmp2 = setxor(frame_neg_tmp1, intersect(frame_neg_tmp1, frame_pos));
-              frame_neg = ismember(frame_neg_tmp1, frame_neg_tmp2);
-              neg = neg_tmp(frame_neg, :);
-              
-          case 'other vehicle'
-              pos = rec_tmp( rec_tmp(:,8)==3, 3:7 );
-              neg_tmp = rec_tmp( rec_tmp(:,8)~=3, 3:7 );
-              frame_pos = pos(:, 1);
-              frame_neg_tmp1 = neg_tmp(:, 1);
-              frame_neg_tmp2 = setxor(frame_neg_tmp1, intersect(frame_neg_tmp1, frame_pos));
-              frame_neg = ismember(frame_neg_tmp1, frame_neg_tmp2);
-              neg = neg_tmp(frame_neg, :);
-              
-          otherwise
-              disp('other object type');
-      end  
-    end
   end
   save([cachedir cls '_train'], 'pos', 'neg');
 end
@@ -116,24 +74,29 @@ function [pos, neg] = pos_neg_init_process(rec_tmp, cls)
 
 end 
 
-function [numpos, pos] = pos_process(pos, numpos, rec_tmp, i, j, img_files, anno_files)
+function [pos, numpos] = pos_process(pos, pos_tmp, numpos, i, img_files, anno_files)
+  [row col] = size(pos_tmp);  
+  for j = 1 : row
       numpos = numpos + 1;
       % extract part before ‘.viratdata.objects.txt’, namely the video name,
       % e.g. filename == VIRAT_S_000001.viratdata.objects.txt
       folder_name = ['/' anno_files(i).name(1 : end-22) '/'];
-      file_name = num2str(rec_tmp(j, 3), '%.6d'); 
+      file_name = num2str(pos_tmp(j, 1), '%.6d'); 
       pos(numpos).im = [img_files folder_name file_name '.jpg'];
-      pos(numpos).x1 = rec_tmp(j, 4);
-      pos(numpos).y1 = rec_tmp(j, 5);
-      pos(numpos).x2 = rec_tmp(j, 4) + rec_tmp(j, 6);
-      pos(numpos).y2 = rec_tmp(j, 5) + rec_tmp(j, 7);
-          
+      pos(numpos).x1 = pos_tmp(j, 2);
+      pos(numpos).y1 = pos_tmp(j, 3);
+      pos(numpos).x2 = pos_tmp(j, 2) + pos_tmp(j, 4);
+      pos(numpos).y2 = pos_tmp(j, 3) + pos_tmp(j, 5);
+  end
 end
 
-function [numneg, neg] = neg_process(neg, numneg, rec_tmp, i, j, img_files, anno_files) 
-  numneg = numneg + 1;
-  folder_name = ['/' anno_files(i).name(1 : end-22) '/'];
-  file_name = num2str(rec_tmp(j, 3), '%.6d'); 
-  neg(numneg).im = [img_files folder_name file_name '.jpg'];
+function [neg, numneg] = neg_process(neg, neg_tmp, numneg, i, img_files, anno_files)
+  [row col] = size(neg_tmp);  
+  for j = 1 : row
+      numneg = numneg + 1;
+      folder_name = ['/' anno_files(i).name(1 : end-22) '/'];
+      file_name = num2str(neg_tmp(j, 1), '%.6d'); 
+      neg(numneg).im = [img_files folder_name file_name '.jpg'];
+  end
 end
   
