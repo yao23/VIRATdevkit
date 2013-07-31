6143cfacbe77ea1fee7cb2e8a8ed29461567594a
Yao <yli44@buffalo.edu>
2013-07-30 22:21:33 -0400

    assign different video ID to PSU and TSU dataset

--- track_virat_trial.m
+++ track_virat_trial.m
@@ -7,18 +7,21 @@
 %         y2)
 
 % video_path = '/home/yao/Desktop/aladdin_repo/tracking_results/VIRAT_Video_Dataset/VIRAT_S_000001/video';
-% video_path = '/home/yao/Desktop/VIRAT_video_cut3/VIRAT_S_000001/';
-video_path = '/home/yao/Desktop/VIRAT_video_cut5/VIRAT_S_000001/';
-% video_path = '/home/yao/Desktop/VIRAT_video_cut6/VIRAT_S_000001/';
+% video_path = '/home/yao/Desktop/VIRAT_video_cut3/VIRAT_S_000001/'; video_id = 5;
+% video_path = '/home/yao/Desktop/VIRAT_video_cut5/VIRAT_S_000001/'; video_id = 5;
+% video_path = '/home/yao/Desktop/VIRAT_video_cut6/VIRAT_S_000001/'; video_id = 5;
 %%% video_path = '/home/yao/Desktop/VIRAT_video_cut3/';
-%%% video_path = '/home/yao/Desktop/VIRAT_video_cut3/VIRAT_S_050202_08_001410_001494/';
-%%% video_path = '/home/yao/Desktop/TSU_Experiments/X_JPG_FORMAT/Vehicle_flee_1'; 
-% video_path = '/home/yao/Projects/object_detection/dataset/PSU_video_cut/ArrestAtMarket_Take#1/left_subclip';
-% video_path = '/home/yao/Projects/object_detection/dataset/PSU_video_cut/CheckingPrisonerInTake#3/left_subclip';
-% video_path = '/home/yao/Projects/object_detection/dataset/PSU_video_cut/JailBreakTake#3/left_subclip';
-% video_path = '/home/yao/Projects/object_detection/dataset/PSU_video_cut/WalkUpDealTake#1/Scene3.1/left_subclip';
-% video_path = '/home/yao/Projects/object_detection/dataset/PSU_video_cut/WalkUpDealTake#1/Scene3.2/left_subclip';
-video_id = 5;
+%%% video_path = '/home/yao/Desktop/VIRAT_video_cut3/VIRAT_S_050202_08_001410_001494/'; video_id = 5;
+video_path = '/home/yao/Desktop/TSU_Experiments/X_JPG_FORMAT/Group_activity'; video_id = 76;
+% video_path = '/home/yao/Desktop/TSU_Experiments/X_JPG_FORMAT/Heavy Box Pick Up'; video_id = 77;
+% video_path = '/home/yao/Desktop/TSU_Experiments/X_JPG_FORMAT/Loading&Unloading_exp'; video_id = 78;
+% video_path = '/home/yao/Desktop/TSU_Experiments/X_JPG_FORMAT/Packages Pick Up'; video_id = 79;
+%%% video_path = '/home/yao/Desktop/TSU_Experiments/X_JPG_FORMAT/Vehicle_flee_1'; video_id = 80;
+% video_path = '/home/yao/Projects/object_detection/dataset/PSU_video_cut/ArrestAtMarket_Take#1/left_subclip'; video_id = 71;
+% video_path = '/home/yao/Projects/object_detection/dataset/PSU_video_cut/CheckingPrisonerInTake#3/left_subclip'; video_id = 72;
+% video_path = '/home/yao/Projects/object_detection/dataset/PSU_video_cut/JailBreakTake#3/left_subclip'; video_id = 73;
+% video_path = '/home/yao/Projects/object_detection/dataset/PSU_video_cut/WalkUpDealTake#1/Scene3.1/left_subclip'; video_id = 74;
+% video_path = '/home/yao/Projects/object_detection/dataset/PSU_video_cut/WalkUpDealTake#1/Scene3.2/left_subclip'; video_id = 75;
 
 % detection_num is used in detection step and track number will be the same
 % detection_num = '30';
@@ -29,11 +32,15 @@ event_path = '/home/yao/Projects/object_detection/tools/VIRATdevkit/models_pas/m
 event_list = '/home/yao/Projects/object_detection/tools/VIRATdevkit/models_pas/threshold_vehicles.txt';
 
 %outcsv_path = '/home/yao/Projects/object_detection/tools/VIRATdevkit/output/detection_pas/VIRAT/VIRAT_S_000001/csv';
-outcsv_path = '/home/yao/Projects/object_detection/tools/VIRATdevkit/output/detection_test/VIRAT5/VIRAT_S_000001/csv';
+%outcsv_path = '/home/yao/Projects/object_detection/tools/VIRATdevkit/output/detection_test/VIRAT5/VIRAT_S_000001/csv';
 %outcsv_path = '/home/yao/Projects/object_detection/tools/VIRATdevkit/output/detection_pas/VIRAT6/VIRAT_S_000001/csv';
 %outcsv_path = '/home/yao/Projects/object_detection/tools/VIRATdevkit/tracking_results/VIRAT/VIRAT_S_000001/csv';
 % outcsv_path = '/home/yao/Desktop/aladdin_repo/tracking_results/VIRAT_Video_Dataset/VIRAT_S_000001/csv';
 %%% outcsv_path = '/home/yao/Projects/object_detection/tools/VIRATdevkit/output/detection_test_all/VIRAT/VIRAT_S_050202_08_001410_001494/csv';
+outcsv_path = '/home/yao/Projects/object_detection/tools/VIRATdevkit/output/detection_pas/TSU_Experiments/Group_activity/csv';
+% outcsv_path = '/home/yao/Projects/object_detection/tools/VIRATdevkit/output/detection_pas/TSU_Experiments/Heavy Box Pick Up/csv';
+% outcsv_path = '/home/yao/Projects/object_detection/tools/VIRATdevkit/output/detection_pas/TSU_Experiments/Loading&Unloading_exp/csv';
+% outcsv_path = '/home/yao/Projects/object_detection/tools/VIRATdevkit/output/detection_pas/TSU_Experiments/Packages Pick Up/csv';
 %%% outcsv_path = '/home/yao/Projects/object_detection/tools/VIRATdevkit/output/detection_pas/TSU_Experiments/Vehicle_flee_1/csv';
 % outcsv_path = '/home/yao/Projects/object_detection/tools/VIRATdevkit/output/detection_pas/PSU/ArrestAtMarket_Take#1/csv';
 % outcsv_path = '/home/yao/Projects/object_detection/tools/VIRATdevkit/output/detection_pas/PSU/CheckingPrisonerInTake#3/csv';
@@ -52,8 +59,8 @@ if ~exist(outtrack_path, 'dir')
 end
 
 %% detection
-virat_processing(video_path, detection_num, default_path, default_list, ...
-  event_path, event_list, outcsv_path, draw);
+% virat_processing(video_path, detection_num, default_path, default_list, ...
+%   event_path, event_list, outcsv_path, draw);
 % aladdin_processing_fix(video_path, detection_num, default_path, default_list, ...
 % event_path, event_list, outcsv_path, draw)
 
@@ -65,11 +72,15 @@ final_main_tracker_slow(video_path, [outcsv_path,'/detection.csv'], [outcsv_path
 %% visualize
 cd /home/yao/Projects/object_detection/tools/VIRATdevkit/visualize/code;
 %outimage_path = '/home/yao/Projects/object_detection/tools/VIRATdevkit/tracking_results/VIRAT/VIRAT_S_000001/track_image';
-outimage_path = '/home/yao/Projects/object_detection/tools/VIRATdevkit/output/detection_test/VIRAT5//VIRAT_S_000001/track_image';
+% outimage_path = '/home/yao/Projects/object_detection/tools/VIRATdevkit/output/detection_test/VIRAT5//VIRAT_S_000001/track_image';
 %outimage_path = '/home/yao/Projects/object_detection/tools/VIRATdevkit/output/detection_test/VIRAT6/VIRAT_S_000001/track_image';
 %%% outimage_path = '/home/yao/Projects/object_detection/tools/VIRATdevkit/tracking_results/VIRAT/VIRAT_S_050202_08_001410_001494/track_image';
+outimage_path = '/home/yao/Projects/object_detection/tools/VIRATdevkit/tracking_results/TSU/Group_activity/track_image'; 
+% outimage_path = '/home/yao/Projects/object_detection/tools/VIRATdevkit/tracking_results/TSU/Heavy Box Pick Up/track_image'; 
+% outimage_path = '/home/yao/Projects/object_detection/tools/VIRATdevkit/tracking_results/TSU/Loading&Unloading_exp/track_image'; 
+% outimage_path = '/home/yao/Projects/object_detection/tools/VIRATdevkit/tracking_results/TSU/Packages Pick Up/track_image'; 
 %%% outimage_path = '/home/yao/Projects/object_detection/tools/VIRATdevkit/tracking_results/TSU/Vehicle_flee_1/track_image'; 
-% outimage_path = '/home/yao/Projects/object_detection/tools/VIRATdevkit/tracking_results/PSU/ArrestAtMarket_Take#1/track_image';
+%outimage_path = '/home/yao/Projects/object_detection/tools/VIRATdevkit/tracking_results/PSU/ArrestAtMarket_Take#1/track_image';
 % outimage_path = '/home/yao/Projects/object_detection/tools/VIRATdevkit/tracking_results/PSU/CheckingPrisonerInTake#3/track_image';
 % outimage_path = '/home/yao/Projects/object_detection/tools/VIRATdevkit/tracking_results/PSU/JailBreakTake#3/track_image';
 % outimage_path = '/home/yao/Projects/object_detection/tools/VIRATdevkit/tracking_results/PSU/WalkUpDealTake#1_Scene3.1/track_image';
